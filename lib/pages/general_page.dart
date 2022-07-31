76f174e0444f50d2a4a91bbdd1b6d25f0e731053
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-07-31 13:57:42 -0500

    Futures 1.8

--- lib/pages/general_page.dart
+++ lib/pages/general_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo_futures/data/data_future.dart';
 
@@ -8,29 +7,31 @@ class GeneralPage extends StatefulWidget {
 }
 
 class _GeneralPageState extends State<GeneralPage> {
-
   DataFuture _dataFuture = DataFuture();
 
   List<Map<String, dynamic>> people = [];
 
   @override
-  initState(){
+  initState() {
     super.initState();
     getData();
   }
 
-  getData(){
+  getData() {
     _dataFuture.getDataList().then((value) {
       people = value;
-      setState((){});
+      setState(() {});
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff2d2d2d),
       appBar: AppBar(
-        title: Text("Integrantes",),
+        title: Text(
+          "Integrantes",
+        ),
       ),
       body: Column(
         children: [
@@ -41,13 +42,28 @@ class _GeneralPageState extends State<GeneralPage> {
           ),
           Expanded(
             child: Container(
-              color: Colors.indigo,
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 14.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(34.0),
+                  topRight: Radius.circular(34.0),
+                ),
+              ),
               child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
                 itemCount: people.length,
-                itemBuilder: (BuildContext context, int index){
+                itemBuilder: (BuildContext context, int index) {
                   return ListTile(
                     title: Text(people[index]["fullName"]),
                     subtitle: Text(people[index]["address"]),
+                    // trailing: Row(
+                    //   mainAxisSize: MainAxisSize.min,
+                    //   children: [
+                    //     IconButton(onPressed: (){}, icon: Icon(Icons.edit),),
+                    //     ElevatedButton(onPressed: (){}, child: Text("Enviar"))
+                    //   ],
+                    // ),
                   );
                 },
               ),
